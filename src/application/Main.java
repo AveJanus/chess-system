807054a2717e030d50a717855db3d7a590c3d3e8
Matheus <archwarlock0@gmail.com>
2023-12-14 10:10:43 -0300

    startint to implement board and piece

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,11 @@
 package application;
 
-import boardgame.Position;
+
+import boardgame.Board;
 
 public class Main {
     public static void main(String[] args){
-        Position p = new Position(2, 4);
-        System.out.println(p);
+
+       Board board = new Board(8, 8);
     }
 }
