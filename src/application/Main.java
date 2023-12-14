d756a6c9a08eb4c85a3f43a098dd6a74012479a4
Matheus <archwarlock0@gmail.com>
2023-12-14 09:57:48 -0300

    First Class: Position

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        Position p = new Position(2, 4);
+        System.out.println(p);
     }
 }
